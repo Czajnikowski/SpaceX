9604b4e62f85332a6d6370d59c971e490cdeeccd
Maciek Czarnik <czajnikowski@gmail.com>
2020-06-07 14:38:37 +0200

    Add initial view controller Eliminate SwiftUI boilerplate

---
+++ SpaceX/LaunchesViewController.swift
@@ -0,0 +1,16 @@
+//
+//  LaunchesViewController.swift
+//  SpaceX
+//
+//  Created by Maciek on 07/06/2020.
+//  Copyright © 2020 Maciek. All rights reserved.
+//
+
+import UIKit
+
+class LaunchesViewController: UIViewController {
+    override func viewDidLoad() {
+        //tmp
+        view.backgroundColor = .red
+    }
+}

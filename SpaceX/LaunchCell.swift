b94593e89d05cf0666e375a1fc8914d3ddd40a44
Maciek Czarnik <czajnikowski@gmail.com>
2020-06-07 14:55:57 +0200

    Implement list fo flights requirement in the most raw form

---
+++ SpaceX/LaunchCell.swift
@@ -0,0 +1,58 @@
+//
+//  LaunchCell.swift
+//  SpaceX
+//
+//  Created by Maciek on 07/06/2020.
+//  Copyright © 2020 Maciek. All rights reserved.
+//
+
+import UIKit
+import SnapKit
+
+struct LaunchCellState {
+    let missionName: String
+    let missionTime: String
+    let missionID: String
+    let rocketName: String
+    let hasReusedPieces: Bool
+}
+
+class LaunchCell: UITableViewCell {
+    private let missionNameLabel = UILabel()
+    private let missionTimeLabel = UILabel()
+    private let missionIDLabel = UILabel()
+    private let rocketNameLabel = UILabel()
+    private let hasReusedPiecesLabel = UILabel()
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        
+        let rootStackView = UIStackView(
+            arrangedSubviews: [
+                missionNameLabel,
+                missionTimeLabel,
+                missionIDLabel,
+                rocketNameLabel,
+                hasReusedPiecesLabel
+            ]
+        )
+        rootStackView.axis = .vertical
+        
+        addSubview(rootStackView)
+        rootStackView.snp.makeConstraints { make in
+            make.edges.equalTo(self).inset(16)
+        }
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func prepare(with state: LaunchCellState) {
+        missionNameLabel.text = state.missionName
+        missionTimeLabel.text = state.missionTime
+        missionIDLabel.text = state.missionID
+        rocketNameLabel.text = state.rocketName
+        hasReusedPiecesLabel.text = "\(state.hasReusedPieces)"
+    }
+}
--- SpaceX/LaunchesViewController.swift
+++ SpaceX/LaunchesViewController.swift
@@ -8,9 +8,46 @@
 
 import UIKit
 
-class LaunchesViewController: UIViewController {
+class LaunchesViewController: UITableViewController {
+    let cellStates = (0 ... 20)
+        .map { "\($0)" }
+        .enumerated()
+        .map {
+            LaunchCellState(
+                missionName: $0.element,
+                missionTime: $0.element,
+                missionID: $0.element,
+                rocketName: $0.element,
+                hasReusedPieces: $0.offset % 2 == 0
+            )
+        }
+    
     override func viewDidLoad() {
-        //tmp
-        view.backgroundColor = .red
+        tableView.register(LaunchCell.self, forCellReuseIdentifier: "temp")
+    }
+    
+    override func tableView(
+        _ tableView: UITableView,
+        numberOfRowsInSection section: Int
+    ) -> Int {
+        return cellStates.count
+    }
+    
+    override func tableView(
+        _ tableView: UITableView,
+        cellForRowAt indexPath: IndexPath
+    ) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(
+            withIdentifier: "temp",
+            for: indexPath
+        )
+        
+        guard let launchCell = cell as? LaunchCell else {
+            return cell
+        }
+        
+        launchCell.prepare(with: cellStates[indexPath.item])
+        
+        return launchCell
     }
 }
